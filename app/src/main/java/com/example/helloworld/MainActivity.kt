a9d539352dfdf3df8e13f06655f41230b180ccef
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2020-10-28 11:58:22 +0530

    Dice roller done!

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,7 +3,9 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,39 @@ import android.widget.Toast
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        val countButton: Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener{count()}
 
     }
+      var randomInt=0
      private fun rollDice()
         {
-            Toast.makeText(this,"You wanna Roll!",Toast.LENGTH_SHORT).show()
+            var randomInt=(1..6).random()
+            result_text.text = randomInt.toString()
+            //Toast.makeText(this,"You wanna Roll!",Toast.LENGTH_SHORT).show()
+            //result_text.text="You Have already Rolled!"
+            var resultText:TextView=findViewById(R.id.result_text)
 
         }
+     private fun count()
+     {
+         if ((randomInt >=1)&&(randomInt<6)) {
+             randomInt += 1
+             result_text.text = randomInt.toString()
+             var resultText:TextView=findViewById(R.id.result_text)
+
+         }
+         else if(randomInt==6)
+         {
+             result_text.text = randomInt.toString()
+             var resultText:TextView=findViewById(R.id.result_text)
+
+         }
+         else
+             randomInt=1
+         result_text.text = randomInt.toString()
+         var resultText:TextView=findViewById(R.id.result_text)
+
+     }
 
 }
